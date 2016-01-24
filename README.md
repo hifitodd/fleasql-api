5fc3bd6513c06eae059464b7bf2831d713589306
Todd DeSantis <hifitodd@gmail.com>
2016-01-24 14:50:12 -0500

    First Commit!

---
+++ README.md
@@ -0,0 +1,1 @@
+# fleasql-api
---
+++ server.js
@@ -0,0 +1,40 @@
+var app = require('express')();
+var http = require('http');
+//var http = require('http').server(app);
+var mysql = require('mysql');
+var bodyParser = require("body-parser");
+var connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'Audiofl3a!!',
+    database: 'books'
+});
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+// LOL testinga
+app.get('/book', function(req, res) {
+    var data = {
+        error: 1,
+        Books: ''
+    };
+
+    connection.query("select * from book", function(err, rows, fields) {
+        if(rows.length != 0) {
+            data.error = 0;
+            data.Books = rows;
+            res.json(data);
+        } else {
+            data.Books = 'No books found.';
+            res.json(data);
+        }
+    })
+});
+
+app.get('/', function (req, res) {
+    res.send('Hello world');
+});
+
+app.listen(3000, function () {
+    console.log('sup fools');
+});
